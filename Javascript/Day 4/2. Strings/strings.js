2e7e545e57b027ee26bd91502ba123c0f585ed51
Cynti84 <115475111+Cynti84@users.noreply.github.com>
2025-02-06 16:55:43 +0300

    updated day 3's work

---
+++ Javascript/Day 4/2. Strings/strings.js
@@ -0,0 +1,81 @@
+import chalk from "chalk"
+//Strings
+
+let myName = "" //empty string
+let myBook = "Introduction to Javascript" //double quotes
+let statement = 'I love to code' //single quotes
+
+//String Properties
+//1. Length - returns the number of characters in a string
+let name = 'Alice'
+console.log(name.length) // 5
+
+//2. charAt() - returns the characer at the specified index in a string
+// s h a k i r a
+// 0 1 2 3 4 5 6
+let fname = "shakira"
+console.log(fname.charAt(3)) // k
+
+//3. concat - concatenates or joins two strings together
+let firstname = "Julia"
+let secondname = "Mwangi"
+console.log(firstname.concat(secondname)) //JuliaMwangi
+// to create a space between the strings
+console.log(firstname.concat(' ' + secondname)) // es5 // output: Julia Mwangi
+console.log(firstname.concat(' ' + "Malik")) //es5 // output: Julia Malik
+console.log(firstname.concat( ` ${secondname}`)) //es6 // output: Julia Mwangi
+
+//4. IndexOf - returns the inded of the first occurance of a specified value in a string
+// I am a student
+// 0123456789.....
+const lname = "Ann Kipkoech Keio"
+console.log(lname.indexOf("K")) // 4
+
+//includes- returns true if a string contains a specified value
+console.log(lname.includes("Keio")) //true
+console.log(lname.includes("keio")) //false
+
+//5. toLowerCase() - converts a string to lowercase letters
+console.log("ELEPHANT".toLowerCase()) // elephant
+let animalName = "Elephant"
+console.log(animalName.toLowerCase())
+
+//6. toUpperCase() - converts a string to uppercase letters
+console.log("elephant".toUpperCase()) // ELEPHANT
+let animal = "elephant"
+console.log(animal.toUpperCase())
+
+//7. Split - splits a string object into an array of strings by separating the strings into sub arrays
+//Eg: performing reverse using split-
+//"money".split("").reverse()
+console.log("wendani".split()) // [ 'wendani]
+console.log("wendani".split(" ")) // [ 'wendani' ]
+console.log("wendani".split("")) // (7) [ 'w', 'e',...]
+
+//8. Substring - extracts character from a string between two specified indices
+// subsring(startingIndex, endingIndex-1)
+let sentence = "I am a student"
+console.log(sentence.substring(7, 11)) // stud - from 7 end at 11-1 ie (10)
+
+//substr - extracts parts of a string
+// beginiing at the charater of the specified position
+// and returns the specified numbers of character
+// substr(statingIndex, numberOfCharacters)
+console.log(sentence.substr(7, 4)) // stud - from 7 index, return 4 characters
+
+let sentence1 = "Hellowz My name is Cee"
+console.log(sentence1.substr(2, 5)) //llowz
+
+//9. trim - removes white spaces from both ends of a string
+let sent = '          Hi I am availabe'
+console.log(sent) //           Hi I am available
+console.log(sent.trim()) // Hi I am available
+//remove white spaces at the start only
+console.log(sent.trimStart()) // Hi I am available
+
+//
+let lname1 = "Jack"
+console.log(lname1.toLowerCase())
+
+
+
---
+++ Javascript/Day 4/Strings practice questions/script.js
@@ -0,0 +1,14 @@
+//Question 1. Check String Input
+function checkString(input) {
+    return typeof input == 'string'
+}
+//Test Data:
+console.log(checkString("w3resource")) // true
+console.log(checkString([1, 2, 4, 0])) // false
+
+//Queastion 2. Check Blank String
+
+function is_Blank(input) {
+    
+}
+
